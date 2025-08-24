246d1c723d6b530f3344d0ee2d8ec4fb2c8a06ba
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-08-24 20:02:34 +0530

    COMPLETE CALCULATOR PROJECT

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
+let calculator;
+let outputArea;
+let equal;
+let reset;
+let backspace;
+let lastInput = [];
+let expression = "";
+let inputArray = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "+",
+  "-",
+  "*",
+  "/",
+  ".",
+];
+
+// Waits for DOM to load before listner starts its job
 document.addEventListener("DOMContentLoaded", () => {
-  const calculator = document.querySelector(".calculator");
-  const outputArea = document.querySelector("div.output>p");
-  const equal = document.getElementById("equal");
-  const reset = document.getElementById("reset");
-  let expression = "";
+  calculator = document.querySelector(".calculator");
+  outputArea = document.querySelector("div.output>p");
+  equal = document.getElementById("equal");
+  reset = document.getElementById("reset");
+  backspace = document.getElementById("backspace");
 
   calculator.addEventListener("click", (e) => {
     let clickInput = e.target.innerText;
-    switch (clickInput) {
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-      case "0":
-      case "+":
-      case "/":
-      case "*":
-      case "-":
-        expression += clickInput;
-        break;
-      case "+/-":
-        negative = false;
-        break;
-    }
+    expression += getInputExpression(clickInput);
     outputArea.innerText = expression;
   });
 
-  equal.addEventListener("click", (e) => {
-    e.stopPropagation();
-    result = getCalculationResult(expression);
-    outputArea.textContent = result;
-    expression = `${result}`;
+  window.addEventListener("keyup", (e) => {
+    console.log(e);
+    switch (true) {
+      case inputArray.includes(e.key):
+        expression += getInputExpression(e.key);
+        outputArea.textContent = expression;
+        break;
+      case e.key === "Backspace":
+        clearLastDigit(expression);
+        break;
+      case e.key === "Enter":
+        getOutputResult(expression);
+        break;
+    }
   });
 
+  // It does the final calculation and gives us the result
+  equal.addEventListener("click", (e) => {
+    e.stopPropagation();
+    getOutputResult(expression);
+  });
+
+  // It will remove last character from our display
+  backspace.addEventListener("click", (e) => {
+    e.stopPropagation();
+    clearLastDigit(expression);
+  });
+
+  // It will reset all the given input
   reset.addEventListener("click", (e) => {
     e.stopPropagation();
     outputArea.textContent = "";
@@ -45,20 +73,57 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function getCalculationResult(expression) {
-  switch (true) {
-    case expression.includes("+"):
-      return calculation(expression.split("+").map(num => Number(num)), "+");
-    case expression.includes("-"):
-      return calculation(expression.split("-").map(num => Number(num)), "-");
-    case expression.includes("*"):
-      return calculation(expression.split("*").map(num => Number(num)), "*");
-    case expression.includes("/"):
-      return calculation(expression.split("/").map(num => Number(num)), "/");
+/** Function is used only when mouse click event occurs */
+function getInputExpression(input) {
+  if (inputArray.includes(input)) {
+    return input;
   }
 }
 
+function getOutputResult(input) {
+  if (input) {
+    result = getCalculationResult(input);
+    outputArea.textContent = result;
+    expression = `${result}`;
+  } else {
+    outputArea.textContent =
+      "No Input Provided.";
+  }
+}
+
+
+/** Calulates the combination of input we have received
+ * i.e. "number operator number" */
+function getCalculationResult(expression) {
+  switch (true) {
+    case expression.includes("+"):
+      return calculation(
+        expression.split("+").map((num) => Number(num)),
+        "+"
+      );
+    case expression.includes("-"):
+      return calculation(
+        expression.split("-").map((num) => Number(num)),
+        "-"
+      );
+    case expression.includes("*"):
+      return calculation(
+        expression.split("*").map((num) => Number(num)),
+        "*"
+      );
+    case expression.includes("/"):
+      return calculation(
+        expression.split("/").map((num) => Number(num)),
+        "/"
+      );
+    case Number(expression) !== NaN:
+      return calculation([Number(expression), lastInput[1]], lastInput[2]);
+  }
+}
+
+/** This function performs all the mathematical calculations requried */
 function calculation(array, operator) {
+  lastInput = [array[0], array[1], operator];
   return array.reduce((num1, num2) => {
     switch (operator) {
       case "+":
@@ -68,7 +133,18 @@ function calculation(array, operator) {
       case "*":
         return num1 * num2;
       case "/":
-        return num1 / num2;
+        if (num2 === 0) {
+          window.alert("I am watching you!");
+          return "";
+        } else {
+          return num1 / num2;
+        }
     }
   });
+}
+
+/** This function will remove the last digit from our output */
+function clearLastDigit(input) {
+  expression = input.substring(0, input.length - 1);
+  outputArea.innerText = expression;
 }
